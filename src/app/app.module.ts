4504f03d5e848b490dae0661b13f296db2534a32
Julio Armenta <elmoxxo123@gmail.com>
2018-12-07 21:16:34 -0700

    JAH Pregunta2
    pregunta 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { Pregunta1Page } from '../pages/pregunta1/pregunta1';
+import { Pregunta2Page } from '../pages/pregunta2/pregunta2';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,7 +16,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     ListPage,
-    Pregunta1Page
+    Pregunta1Page,Pregunta2Page
   ],
   imports: [
     BrowserModule,
@@ -26,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     ListPage,
-    Pregunta1Page
+    Pregunta1Page,Pregunta2Page
   ],
   providers: [
     StatusBar,
